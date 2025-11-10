012bbf9d6bd7afba75fc8287a584820472f3420b
Ludwig Lind <ludwig.lind@physik.tu-darmstadt.de>
2025-11-10 17:47:19 +0100

    Add scipy approach.
    Problematic due to discrete scoreboard

--- Dartboard/vers0.py
+++ Dartboard/vers0.py
@@ -7,6 +7,7 @@ Created on Mon Nov 10 16:53:29 2025
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 def gaussian(x, y, sigma, x0, y0):
     A = np.exp(-(x-x0)**2/(2*sigma**2))* np.exp(-(y-y0)**2/(2*sigma**2))
@@ -56,26 +57,31 @@ imxy = ax.imshow(scoreboard,
                     aspect=1,
                     extent=extent_xy)
 
-sigma, x0, y0 = 0.01, 0, 0
 
-throw = gaussian(X, Y, sigma, x0, y0)
 
-fig2,ax2 = plt.subplots()
+# throw = gaussian(X, Y, sigma, x0, y0)
 
-imxy = ax2.imshow(throw,
-                    cmap='hot',
-                    origin='lower',
-                    aspect=1,
-                    extent=extent_xy)
+# fig2,ax2 = plt.subplots()
 
-result = throw*scoreboard
+# imxy = ax2.imshow(throw,
+#                     cmap='hot',
+#                     origin='lower',
+#                     aspect=1,
+#                     extent=extent_xy)
 
-fig3,ax3 = plt.subplots()
+def neg_score(params):
+    x0, y0 = params
+    throw = gaussian(X, Y, sigma, x0, y0)
+    result = throw*scoreboard
+    return(-np.sum(result))
 
-imxy = ax3.imshow(result,
-                    cmap='hot',
-                    origin='lower',
-                    aspect=1,
-                    extent=extent_xy)
+sigma = 0.01
 
-print(np.sum(result))
+initial_guess = [1,0]
+
+optimal = minimize(neg_score, initial_guess)
+
+x0_opt, y0_opt = optimal.x
+max_score = -optimal.fun
+
+print('Max Score ' + str(round(max_score,3)) + ' at x = ' + str(round(x0_opt,3)) + ' and y = ' + str(round(x0_opt,2)))
